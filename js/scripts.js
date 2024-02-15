a16db54a880e2f7ca8745f631552aa8e5408c7f9
Anderson <AndersonTsaiTW@gmail.com>
2024-02-16 06:26:43 +0800

    Rewrite scropts.js 1. clean doc 2. create an pokeman list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,42 @@
-alert('Hello world')
+let pokemonList = [];
 
-let favoriteFood = 'StinkyTofu';
-document.write(favoriteFood);
+pokemonList[0] = {};
+pokemonList[0].order = 1;
+pokemonList[0].name = 'Bulbasaur';
+pokemonList[0].height = 0.7;
+pokemonList[0].weight = 6.9;
+pokemonList[0].types = ['grass', 'poison'];
+pokemonList[0].evolve = true;
+
+pokemonList[1] = {};
+pokemonList[1].order = 3;
+pokemonList[1].name = 'Venusaur';
+pokemonList[1].height = 2;
+pokemonList[1].weight = 100;
+pokemonList[1].types = ['grass', 'poison'];
+pokemonList[1].evolve = false;
+
+pokemonList[2] = {};
+pokemonList[2].order = 6;
+pokemonList[2].name = 'Charizard';
+pokemonList[2].height = 1.7;
+pokemonList[2].weight = 90.5;
+pokemonList[2].types = ['fire', 'flying'];
+pokemonList[2].evolve = false;
+
+pokemonList[3] = {};
+pokemonList[3].order = 19;
+pokemonList[3].name = 'Rattata';
+pokemonList[3].height = 0.3;
+pokemonList[3].weight = 3.5;
+pokemonList[3].types = ['normal'];
+pokemonList[3].evolve = true;
+
+pokemonList[4] = {};
+pokemonList[4].order = 20;
+pokemonList[4].name = 'Raticate';
+pokemonList[4].height = 0.7;
+pokemonList[4].weight = 18.5;
+pokemonList[4].types = ['normal'];
+pokemonList[4].evolve = false;
 
