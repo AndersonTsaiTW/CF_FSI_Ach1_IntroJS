e7ed6187852aaafba79b8d94f714ede9125b738f
Anderson <AndersonTsaiTW@gmail.com>
2024-02-24 14:38:00 +0800

    update scripts.js 1. turn pokemonList to IIEF 2. add function getOne to show the pokemon data by the name

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonRepository = (function () {
+
     let pokemonList = []; // empty array
     pokemonList[0] = {order : 1, name : 'Bulbasaur', height : 0.7, weight : 6.9, types : ['grass', 'poison'], evolve : true};
     pokemonList[1] = {order : 3, name : 'Venusaur', height : 2, weight : 100, types : ['grass', 'poison'], evolve : false};
@@ -8,17 +9,33 @@ let pokemonRepository = (function () {
 
     return {
       add: function(pokemon) {
-        pokemonList.push(pokemon);
+        if (typeof pokemon === 'object' && 
+        Object.keys(pokemon).join() === Object.keys(pokemonList[0]).join()) {
+            // here using the array.join() to change the objectKeys(array) to string
+            // because object and array are not equal even though they look totally the same
+            pokemonList.push(pokemon);
+            console.log('Add ' + pokemon.name + ' to the list successfully.')
+        } else {
+            console.log('It is not complete pokemon data!')
+        }
       },
       getAll: function() {
         return pokemonList;
-      }
+      },
+      getOne: function(name) {
+        pokemonList.forEach(function(pokemon) {
+            if (pokemon.name === name) {
+                console.log(pokemon)
+            }
+        })
+    }
     };
-  })();
+})();
   
   console.log(pokemonRepository.getAll()); // []
-  // pokemonRepository.add({ name: 'Pikachu' });
-  // console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+  pokemonRepository.add({ name: 'Pikachu' });
+  pokemonRepository.add({ order : 8, name : 'Kameil', height : 1, weight : 22.5, types : ['water'], evolve : true});
+  console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
 
 let heightMax = 0;
 pokemonRepository.getAll().forEach(function(user) {
@@ -32,3 +49,6 @@ pokemonRepository.getAll().forEach(function(user) {
         document.write(' -- This is the BIGGEST pokemon!!!')
     }
 })
+
+setTimeout(choose = prompt("Please input the pokemon's mane."), 3000); // Run this in 1000ms
+setTimeout(pokemonRepository.getOne(choose),3000);
