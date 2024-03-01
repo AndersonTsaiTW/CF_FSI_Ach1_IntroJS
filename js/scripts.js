b07fb90107a9cf0bb880a292a4b57477941f8461
Anderson <AndersonTsaiTW@gmail.com>
2024-03-01 08:34:45 +0800

    update to ex 1.7: 1. Update to load the pokemon's API to catch data: Load the list to get the name and load the detail to get the height, types, and img 2. Use forEach to pick up the types' name 3. Create show/hide loading message and fix it above the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,9 @@
 let pokemonRepository = (function () {
-
     let pokemonList = []; // empty array
-    pokemonList[0] = {order : 1, name : 'Bulbasaur', height : 0.7, weight : 6.9, types : ['grass', 'poison'], evolve : true};
-    pokemonList[1] = {order : 3, name : 'Venusaur', height : 2, weight : 100, types : ['grass', 'poison'], evolve : false};
-    pokemonList[2] = {order : 6, name : 'Charizard', height : 1.7, weight : 90.5, types : ['fire', 'flying'], evolve : false};
-    pokemonList[3] = {order : 19, name : 'Rattata', height : 0.3, weight : 3.5, types : ['normal'], evolve : true};
-    pokemonList[4] = {order : 20, name : 'Raticate', height : 0.7, weight : 18.5, types : ['normal'], evolve : false};
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
     function add(pokemon) {
-        if (typeof pokemon === 'object' && 
-        Object.keys(pokemon).join() === Object.keys(pokemonList[0]).join()) {
-            // here using the array.join() to change the objectKeys(array) to string
-            // because object and array are not equal even though they look totally the same
+        if (typeof pokemon === 'object' && 'name' in pokemon) {
             pokemonList.push(pokemon);
             console.log('Add ' + pokemon.name + ' to the list successfully.')
         } else {
@@ -41,32 +33,89 @@ let pokemonRepository = (function () {
         });
         // return pokemon.name;
     }
-    function showDetails(pokemon) {
-        console.log(pokemon);
+    function loadList() {
+        showLoadingMessage();
+        return fetch(apiUrl).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            json.results.forEach(function(item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                console.log(pokemon);
+            });
+            hideLoadingMessage();
+        }).catch(function(e) {
+            console.error(e);
+            hideLoadingMessage();
+        })
     }
+    function loadDetails(item) {
+        showLoadingMessage();
+        let url = item.detailsUrl;
+        let typesList = [];
+        return fetch(url).then(function(response) {
+            return response.json();
+        }).then(function(details) {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            // item.types = details.types;  //This is the original code
+            details.types.forEach(function(content){
+                // console.log(content.type.name);
+                typesList.push(content.type.name);
+            })
+            item.types = typesList;
+            hideLoadingMessage();
+        }).catch(function(e) {
+            console.error(e);
+            hideLoadingMessage();
+        })
+    }
+    function showDetails(pokemon) {
+        pokemonRepository.loadDetails(pokemon).then(function() {
+        console.log(pokemon);
+    });
+    }
+    function showLoadingMessage() {
+        // Create a loading message element
+        let loadingMessage = document.createElement('div');
+        loadingMessage.innerText = 'Loading...';
+        loadingMessage.id = 'loading-message';
+        loadingMessage.style.position = 'fixed';
+        loadingMessage.style.top = '0';
+        loadingMessage.style.left = '0';
 
+        // Append the loading message to the body or a specific container
+        document.body.appendChild(loadingMessage);
+    }
+    function hideLoadingMessage() {
+        // Remove the loading message element from the DOM
+        let loadingMessage = document.getElementById('loading-message');
+        if (loadingMessage) {
+            loadingMessage.remove();
+        }
+    }
     return {
         add: add,
         getAll: getAll,
         getOne: getOne,
-        addListItem: addListItem
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails,
+        showLoadingMessage: showLoadingMessage,
+        hideLoadingMessage: hideLoadingMessage
     };
 })();
-  
-  console.log(pokemonRepository.getAll()); // []
-  pokemonRepository.add({ name: 'Pikachu' });
-  pokemonRepository.add({ order : 8, name : 'Kameil', height : 1, weight : 22.5, types : ['water'], evolve : true});
-  console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
 
-let heightMax = 0;
-pokemonRepository.getAll().forEach(function(user) {
-    if (heightMax < user.height) {
-        heightMax = user.height
-    }
-})
-pokemonRepository.getAll().forEach(function(user) {
-    pokemonRepository.addListItem(user);
-})
+
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(user) {
+        pokemonRepository.addListItem(user);
+    });
+});
 
 // setTimeout(choose = prompt("Please input the pokemon's mane."), 3000);
 // setTimeout(pokemonRepository.getOne(choose),3000);
